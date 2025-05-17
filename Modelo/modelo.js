f2e7b50174f00676cd0133f697a6831bb7c638d4
Valentin Rodriguez <valenrodriguez910@gmail.com>
2025-05-17 20:24:35 -0300

    Se generan las clases a utilizar

---
+++ Modelo/modelo.js
@@ -0,0 +1,41 @@
+class Producto {
+    id;
+    marca;
+    precio;
+    imagen; // URL
+    activo; // booleano: true/false
+
+    constructor(id, marca, precio, imagen, activo) {
+        this.id = id;
+        this.marca = marca;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.activo = activo;
+    }
+}
+
+class Celular extends Producto {
+    modelo;
+    color;
+    almacenamiento; // en GB
+    ram; // en GB
+
+    constructor(id, marca, precio, imagen, activo, marca, modelo, color, almacenamiento, ram) {
+        super(id, marca, precio, imagen, activo);
+        this.modelo = modelo;
+        this.color = color;
+        this.almacenamiento = almacenamiento;
+        this.ram = ram;
+    }
+}
+
+class Accesorio extends Producto {
+    tipo; // ej: "Funda", "Auricular", "Cargador"
+    compatibilidad; // ej: "iPhone 13", "Universal", etc.
+
+    constructor(id, marca, precio, imagen, activo, tipo, compatibilidad) {
+        super(id, marca, precio, imagen, activo);
+        this.tipo = tipo;
+        this.compatibilidad = compatibilidad;
+    }
+}
