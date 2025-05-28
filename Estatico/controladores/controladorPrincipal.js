fca1110b3f54ad07d166efaf37c5cadf56b591b1
Rodriguez, Valentin <valentin.rodriguez@accusys.com.ar>
2025-05-28 13:44:38 -0300

    se agrega el llamado a la API en el controladorPrincipal Estatico del Cliente

--- Estatico/controladores/controladorPrincipal.js
+++ Estatico/controladores/controladorPrincipal.js
@@ -4,16 +4,42 @@ import { Accesorio } from "../modelos/accesorio.js";
 class ControlPrincipal{
     Modelo;
     VistaPrincipal;
-    constructor(p_modelo, p_vista, datos_bd_json){
+    constructor(p_modelo, p_vista){
         this.Modelo = p_modelo;
         this.VistaPrincipal = p_vista;
-        //this.cargaInicial(datos_bd_json);
+        //this.obtenerProductos()
         this.registrarControlador();
     }
 
-    cargaInicial(array) {
 
-        this.Modelo.Productos = array.map(
+    obtenerProductos() {
+        const spinner = document.getElementById('spinner-container');
+        spinner.style.display = 'flex';
+
+        fetch('http://localhost:3036/api/productos')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error en la solicitud: ' + response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const productos = createProductos(data);
+                dibujarProductos(productos);
+            })
+            .catch(error => {
+                console.error('Error en la peticion', error);
+                alert('Error al cargar los productos.\n\nVuelva a intentarlo en otro momento:(');
+            })
+            .finally(() => {
+                spinner.style.display = 'none';
+            });
+    }
+
+    createProductos(data) {
+
+        this.Modelo.Productos = data.map(
             (x)=>{
                 if(x.modelo){
                     return new Celular(x.id, x.marca, x.precio, x.imagen, x.activo , x.modelo, x.color, x.almacenamiento, x.ram);
@@ -22,6 +48,64 @@ class ControlPrincipal{
                 }
             }
         );
+    }
+
+    dibujarProductos(productos){
+        const panelCelulares = document.getElementById('panel-derecho-celulares')
+        const panelAccesorios = document.getElementById('panel-derecho-accesorios')
+
+        productos.forEach(prod => {
+            const div = document.createElement('div');
+            div.classList.add('producto');
+
+            const img = document.createElement('img');
+            img.src = prod.imagen;
+            prod instanceof Celular ? img.alt = 'Imagen de Celular' : img.alt = 'Imagen de Accesorio'
+            img.classList.add('img-producto');
+
+            const name = document.createElement('h3') 
+            prod instanceof Celular ? name.textContent = prod.marca + ' ' + prod.modelo : name.textContent = prod.tipo + ' ' + prod.marca
+
+            if (prod instanceof Celular){
+                let p_color = document.createElement('p');
+                p_color.textContent = prod.color;
+
+                let p_almacenamiento = document.createElement('p');
+                p_almacenamiento.textContent = prod.almacenamiento;
+
+                let p_ram = document.createElement('p');
+                p_ram.textContent = prod.ram;
+
+            } else{
+                let p_tipo = document.createElement('p');
+                p_tipo.textContent = prod.tipo;
+
+                let p_compatibilidad = document.createElement('p');
+                p_compatibilidad.textContent = prod.compatibilidad;
+            }
+
+            const p_precio = document.createElement('p');
+            p_precio.textContent = prod.precio;
+
+            const btnCarrito = document.createElement('button');
+            btnCarrito.textContent = 'Agregar al carrito'
+            btnCarrito.classList.add('btn-carrito');
+
+            div.appendChild(img);
+            div.appendChild(name);
+            if (prod instanceof Celular){
+                div.appendChild(p_color);
+                div.appendChild(p_almacenamiento);
+                div.appendChild(p_ram);
+            } else {
+                div.appendChild(p_tipo);
+                div.appendChild(p_compatibilidad);
+            }
+            div.appendChild(p_precio);
+            div.appendChild(btnCarrito);
+
+            prod instanceof Celular ? panelCelulares.appendChild(div) : panelAccesorios.appendChild(div);
+        });
     }
 
     registrarControlador(){
@@ -39,12 +123,6 @@ class ControlPrincipal{
             document.getElementById("panel-derecho-celulares").style.display = "grid";
             document.getElementById("panel-derecho-accesorios").style.display = "none";
 
-            /*const arrayCelulares = this.Modelo.Productos.filter(producto => {
-                return producto instanceof Celular;
-            })*/
-
-            //this.Vista.dibujarFilas(arrayCelulares);
-            
         })
 
         this.VistaPrincipal.panelIzquierdo.imgAccesorio.addEventListener("click", (e) =>{
@@ -52,17 +130,7 @@ class ControlPrincipal{
             document.getElementById("panel-derecho-celulares").style.display = "none";
             document.getElementById("panel-derecho-accesorios").style.display = "grid";
 
-            /*const arrayAccesorios = this.Modelo.Productos.filter(producto => {
-                return producto instanceof Accesorio;
-            })*/
-
-            //this.Vista.dibujarFilas(arrayAccesorios);
-            
         })
-    }
-
-    dibujarFilas(){
-        
     }
 
 }
